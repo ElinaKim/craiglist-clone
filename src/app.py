a270547414895e52a5240378511e6139fad2d3a3
Elina Kim <elinakim791@gmail.com>
2023-02-06 11:25:25 -0800

    connected SQLAlchemy

--- src/app.py
+++ src/app.py
@@ -1,10 +1,25 @@
 from flask import Flask
+import psycopg2
+from flask_sqlalchemy import SQLAlchemy
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgrespw@localhost:55000/craigslist"
+app.config['SQLALCHEMY_TRACKMODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+#conn_str = os.getenv("CONN_STR")
+
 
 @app.route('/')
 def index():
     return 'Hello World'
+
 
 if __name__ == '__main__':
     app.run()
